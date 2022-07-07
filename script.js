f88dd8d5557350054ff329f28eb322c24411f1b7
Miguel Penha <miguel.penha1@gmail.com>
2022-07-07 12:15:19 -0300

    Adicionando Images Grid

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 function showOrHiddenMenu() {
-    const menu = window.document.getElementById('menu')
+    const menu = document.getElementById('menu')
 
-    if (menu.className === 'hidden') {
-        menu.className = ''
+    if (menu.className.includes('hidden')) {
+        menu.classList.remove('hidden')
     } else {
         menu.style.animation = 'hiddenRight 1s'
         
         setTimeout(() => {
-            menu.className = 'hidden'
+            menu.classList.add('hidden')
             menu.style.animation = 'showLeft 1s'
         }, 1000)
     }
 }
+
+function loadImages() {
+    const images = document.querySelectorAll('.container-images-grid>.column>img')
+
+    for (let image of images) {
+        image.style.visibility = 'hidden'
+    }
+
+    setInterval(() => {
+        for (let image of images) {
+            if (image.getBoundingClientRect().top <= window.innerHeight) {
+                image.style.animation = 'bottomOnTop 1s linear'
+                image.style.visibility = 'visible'
+            }
+        }
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', () => loadImages())
